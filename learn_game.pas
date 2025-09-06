1a4496c93c80089095deaa5800acc4e9b284a2b1
Maxim-L91 <maximrus62@gmail.com>
2025-09-06 19:37:43 +0300

    refactor: Игровое меню объеденино в цикл
    Раньше меню оставалось вверху. Теперь, после каждого обновления меню появляется внизу и его всегда видно.

--- learn_game.pas
+++ learn_game.pas
@@ -2,8 +2,10 @@ program learn_game;
 var
 	{Меню}
 	choice: byte;
+
 	{Имя персонажа}
 	character_name: string;
+
 	{Статы персонажа}
 	strength: word = 10;
 	health: word = 10;
@@ -11,23 +13,40 @@ var
 	regeneration: word = 10;
 	energy: word = 10;
 	amount_stats: integer;
+
 begin
 	writeln('Введите имя персонажа:');
 	readln(character_name);
-	writeln('1. Мой персонаж');
-	readln(choice);
-	if choice = 1 then
-	begin
-		writeln('Имя: ', character_name);
-		writeln('Сила: ', strength);
-		writeln('Здоровье: ', health);
-		writeln('Броня: ', armor);
-		writeln('Регенерация: ', regeneration);
-		writeln('Энергия: ', energy);
-		amount_stats := strength + health + armor + regeneration + energy;
-		writeln('Сумма: ', amount_stats )
-	end
-	else
-		writeln('Такой команды не существует');
-end.
 
+	{Главное меню}
+	repeat
+		writeln(' ');
+		writeln('1. Мой персонаж');
+		writeln('2. Бой');
+		writeln('0. Выход');
+		readln(choice);
+		
+		{Меню персонажа}
+		if choice = 1 then
+		begin
+			writeln(' ');
+			writeln('Имя: ', character_name);
+			writeln(' ');
+			writeln('Сила: ', strength);
+			writeln('Здоровье: ', health);
+			writeln('Броня: ', armor);
+			writeln('Регенерация: ', regeneration);
+			writeln('Энергия: ', energy);
+			amount_stats := strength + health + armor + regeneration + energy;
+			writeln('Сумма: ', amount_stats);
+		end 
+		else if choice = 0 then
+		begin
+			writeln('Вы вышли из игры');
+		end
+		else
+		begin
+			writeln('Такой команды не существует');
+		end
+	until choice = 0;
+end.
